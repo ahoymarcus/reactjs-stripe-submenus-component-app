847103e1a4fc31c49cb19490c8751ced6310585b
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-26 11:37:59 -0300

    Iniciar repositorio local

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// https://www.youtube.com/watch?v=ly3m6mv5qvg
+// 5 hr 44' 30 ''
+import React from 'react';
+
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h2>stripe submenus setup</h2>
+    </>
+  )
+}
+
+
+
+export default App
+
+
