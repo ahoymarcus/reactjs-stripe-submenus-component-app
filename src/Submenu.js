e1d818c76755de70536cadf06a629efcbfedc0cf
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-26 21:53:57 -0300

    Criar um variavel de estado em Submenu.js e usar para controlar o numero de colunas para ser setado cada submenu de acordo com a propriedade CSS-Grid.

--- src/Submenu.js
+++ src/Submenu.js
@@ -11,9 +11,14 @@ const Submenu = () => {
     page: { page, links } 
   } = useGlobalContext();
   const container = useRef(null);
+  const [ columns, setColumns ] = useState('col-2');
+
 
 
   useEffect(() => {
+    // IMPORTANTE: setar default no início
+    setColumns('col-2');
+
     // pegar referência do element node
     const submenu = container.current;
     const { center, bottom } = location;
@@ -21,6 +26,12 @@ const Submenu = () => {
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
 
+    if (links.length === 3) {
+      setColumns('col-3');
+    }
+    if (links.length > 3) {
+      setColumns('col-4');
+    }
   }, [location]);
 
 
@@ -32,7 +43,7 @@ const Submenu = () => {
       ref={container}
     >
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`} >
+      <div className={`submenu-center ${columns}`} >
         {links.map((link, index) => {
           const { label, icon, url } = link;
 
